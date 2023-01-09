9ef8b9ae6328718104a9834f54b64b5af92995e8
jonghyeon baek <whdgus928@naver.com>
2023-01-09 23:38:50 +0900

    Update 2_📢가뭄 지수 정보 조회.py

--- pages/2_📢가뭄 지수 정보 조회.py
+++ pages/2_📢가뭄 지수 정보 조회.py
@@ -45,7 +45,7 @@ filePath, fileName = os.path.split(__file__)
 data_path = os.path.join(filePath,'using_data','행정코드.csv')
 
 st.set_page_config(
-    page_title = "⛔위기 대응 프로젝트",
+    page_title = "위기 대응 플랫폼",
     layout = 'wide'
 )
 # 📜
